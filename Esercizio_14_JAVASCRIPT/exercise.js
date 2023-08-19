67e3055612370e8ba6c31465e1e61853c607e872
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-08-19 02:46:01 +0200

    add exercise 14

---
+++ Esercizio_14_JAVASCRIPT/exercise.js
@@ -0,0 +1,73 @@
+function uncompletedNotes(notes) {
+  var uncompleted = [];
+
+  notes.forEach(note => {
+    var uncompletedTodos = note.todos.filter(todo => !todo.done);
+    if (uncompletedTodos.length > 0) {
+      uncompleted.push({
+        id: note.id,
+        description: note.description,
+        todos: uncompletedTodos,
+      });
+    }
+  });
+
+  return uncompleted;
+}
+
+const notes = [
+  {
+    id: 1,
+    description: "Workout program",
+    todos: [
+      {
+        id: 1,
+        name: "Push ups - 10 x 3",
+        done: false,
+      },
+      {
+        id: 2,
+        name: "Abdominals - 20 x 3",
+        done: true,
+      },
+      {
+        id: 3,
+        name: "Tapis Roulant - 15min",
+        done: true,
+      },
+    ],
+  },
+  {
+    id: 2,
+    description: "Front-end Roadmap",
+    todos: [
+      {
+        id: 1,
+        name: "Learn HTML",
+        done: true,
+      },
+      {
+        id: 2,
+        name: "Learn CSS",
+        done: true,
+      },
+      {
+        id: 3,
+        name: "Learn JavaScript",
+        done: true,
+      },
+      {
+        id: 4,
+        name: "Learn Angular",
+        done: false,
+      },
+    ],
+  },
+];
+
+
+
+const uncompletedNote = uncompletedNotes(notes);
+console.log(uncompletedNote)
+
+
